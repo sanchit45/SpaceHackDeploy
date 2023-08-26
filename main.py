e4e64e3238afddbc7dc7d763f604066291500910
Sanchit Baweja <99529694+sanchit45@users.noreply.github.com>
2023-08-26 08:05:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 #configuringthe origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://ravinainn.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
